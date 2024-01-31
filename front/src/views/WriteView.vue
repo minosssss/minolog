966c9fc27f634f52d5f522c15ab15b1e09771b4f
MINHO LEE <mh.lee239@gmail.com>
2024-01-31 20:58:08 +0900

    Vue Init

---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {defineComponent, ref} from "vue";
+
+const count = ref(0);
+console.log(count);
+</script>
+
+<template>
+  <div>
+    <input type="text" placeholder="제목 입력해주세요."/>
+    <textarea rows="15"></textarea>
+  </div>
+  <button>글 작성!</button>
+</template>
+
+<style scoped>
+
+</style>
